5f77d4465e111498e3972093e23eda1955624c4b
Madeeeba <1-800-fuqme@proton.me>
2025-08-07 08:07:31 +0300

    first commit

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    layout="centered",
+    page_title="Home",
+    page_icon=":champagne:",
+    initial_sidebar_state="expanded"
+)
---
+++ pages/1_EDA.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    layout="centered",
+    page_title="EDA",
+    page_icon=":writing_hand:",
+    initial_sidebar_state="collapsed"
+)
